16277d7525c211f7f836e8c191e9c3650b857578
Adam Hyland <protonk@gmail.com>
2012-03-08 02:16:05 -0600

    add dependency to zoo. Add moving average option to ratings/day plot

--- pom/analysis.pom.R
+++ pom/analysis.pom.R
@@ -8,11 +8,30 @@ library(ggplot2)
 
 # Show the growth from the start for the ratings as a whole
 
-growFromSt <- function() {
+growFromSt <- function(method = "Raw") {
+  # Initial recorded ratings per day
   fromstart <- ddply(price.df, .(price.df[, "Date"]), "nrow")
-  names(fromstart) <- c("Date", "Ratings")
-  ggplot(data = fromstart, aes(Date, Ratings)) + geom_line() +
-    scale_x_date(name = '') + scale_y_continuous(name = "Ratings per Day")
+  # Next few lines fill in the blanks so we get an 
+  # equally spaced series including days w/ no ratings
+  # If you see sum(diff(fromstart[,1])) - nrow(fromstart)
+  # we miss 94 days w/o this
+  dateseq <- seq(fromstart[1, 1], fromstart[nrow(fromstart), 1], by = 1)
+  fulldate.df <- data.frame(matrix(cbind(0,0), length(dateseq), 2), stringsAsFactors = FALSE)
+  fulldate.df[, 1] <- dateseq
+  fulldate.df[which(fulldate.df[, 1] %in% fromstart[ , 1]), 2] <- fromstart[ , 2]
+  names(fulldate.df) <- c("Date", "Ratings")
+  # Convert to time series so we can get some moving averages
+  rate.ts <- ts(fulldate.df[, 2], start = fromstart[1, 1], end = fromstart[nrow(fromstart), 1])
+  # rollmean() is a little more tractable than the other functions in base
+  library(zoo)
+  fulldate.df$Filtered <- as.numeric(c(rep(NA, 3), rollmean(rate.ts, k = 4, align = "center")))
+  switch(method,
+         Raw = ggplot(data = fulldate.df, aes(Date, Ratings)) + geom_line() +
+           scale_x_date(name = '') + scale_y_continuous(name = "Ratings per Day"),
+         MA = ggplot(data = fulldate.df, aes(Date, Ratings)) + geom_line(alpha = 0.7) +
+           scale_x_date(name = '') + scale_y_continuous(name = "Ratings per Day") +
+           geom_line(aes(Date, Filtered), colour = "blue", size = 1.3)
+         )
 }
 
 # Ratings by day of the month. Excluding the big jump for Jan 27, 2012 there isn't too 
